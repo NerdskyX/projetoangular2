5c25971bb5917560dc35a0c4877be13cf46be868
Vitor A. Rinkawetsky <88173262+NerdskyX@users.noreply.github.com>
2023-05-09 15:21:02 -0300

    bvcb

---
+++ src/environment.ts
@@ -0,0 +1,17 @@
+export const environment = {
+
+  production: false,
+
+  firebaseConfig: {
+
+  apiKey: "AIzaSyB_um8dlx7O9n3Um9KKPqS_99gpFipeTqE",
+  authDomain: "crud-angular-firebase-33150.firebaseapp.com",
+  databaseURL: "https://crud-angular-firebase-33150-default-rtdb.firebaseio.com",
+  projectId: "crud-angular-firebase-33150",
+  storageBucket: "crud-angular-firebase-33150.appspot.com",
+  messagingSenderId: "615901931209",
+  appId: "1:615901931209:web:0c00350edb135889cd0f18"
+
+  }
+
+};
